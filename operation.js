8feee59c24fb0d16d5f60d5da0790844a611f0f0
PalakPartani <partanipalak16@gmail.com>
2020-07-14 02:01:56 -0700

    ADDED:MediaQuery to make UI responsive

--- operation.js
+++ operation.js
@@ -1,3 +1,5 @@
+var menu = true;
+
 var users = [{ Id: 0, Name: "Palak", Email: "ppartani@gmail.com", Status: "Active", Type: "User", LastSeen: "2d Ago", Permission: "Valid" },
 { Id: 1, Name: "Palak", Email: "ppartani@gmail.com", Status: "Active", Type: "User", LastSeen: "2d Ago", Permission: "Valid" },
 { Id: 2, Name: "Palak", Email: "ppartani@gmail.com", Status: "Active", Type: "User", LastSeen: "2d Ago", Permission: "Valid" },
@@ -10,7 +12,7 @@ text += "<table>";
 text += "<tr><th> Name</th><th>Email</th> <th>Status</th><th>Type</th><th>Last Login</th><th>Permission</th> </tr > ";
 
 for (i = 0; i < users.length; i++) {
-    text += "<tr><td>" + users[i].Id + "-" + users[i].Name + "</td><td>" + users[i].Email + "</td>";
+    text += "<tr><td>" + users[i].Name + "</td><td>" + users[i].Email + "</td>";
     if (users[i].Status.match("Active")) {
         text += "<td><input class='statusButton' type='button' button name='active' value='Active' id='btn' onclick='changeStatus(" + i + ",`InActive`)'></td>";
 
@@ -26,7 +28,7 @@ document.getElementById("table").innerHTML = text;
 document.getElementById("count").innerHTML = users.length;
 
 function changeStatus(Id, Status) {
-    users.map((data, status) => {
+    users.map((data) => {
         if (data.Id == Id) {
             data.Status = Status;
         }
@@ -36,7 +38,7 @@ function changeStatus(Id, Status) {
     textNew += "<table>";
     textNew += "<tr><th> Name</th><th>Email</th> <th>Status</th><th>Role</th><th>Last Login</th><th>Permission</th> </tr > ";
     for (i = 0; i < users.length; i++) {
-        textNew += "<tr><td>" + users[i].Id + "-" + users[i].Name + "</td><td>" + users[i].Email + "</td>";
+        textNew += "<tr><td>" + users[i].Name + "</td><td>" + users[i].Email + "</td>";
         if (users[i].Status == "Active") {
             textNew += "<td><input class='statusButton' type='button' button name='active' value='Active' id='btn' onclick='changeStatus(" + i + ",`InActive`)'></td>";
 
@@ -53,4 +55,20 @@ function changeStatus(Id, Status) {
     // return s;
     // if (status.value == "Active") status.value = "Inactive";
     // else elem.value = "Inactive";
+}
+
+
+
+
+function menuOption() {
+    if (menu) {
+        document.getElementById("verticalMenu").style.display = "block";
+        menu = false
+
+    } else {
+        document.getElementById("verticalMenu").style.display = "none";
+        menu = true
+
+    }
+
 }
